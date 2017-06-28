6bfb10aa4b352a4f4d5f426eaa3b0a5e3eb49310
Tran Situ <tsitu13@gmail.com>
2017-06-27 21:42:20 -0700

    Stopped tracking files in .gitignore

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import DemoApp from './components/demo/DemoApp';
 import App from './components/App';
 
 ReactDOM.render(
-  // <DemoApp />,
   <App />,
   document.getElementById('container'),
 );
